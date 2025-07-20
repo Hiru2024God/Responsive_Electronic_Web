6ddf67b1fda3975fdc04777e34e99eb3ca8da65d
Hirusha jayawardena <175980607+Hiru2024God@users.noreply.github.com>
2025-07-21 01:16:26 +0530

    Admin Side Design Done

---
+++ Admin/script.js
@@ -0,0 +1,15 @@
+function Adminpanel(){
+    window.location = "Adminpanel.html";
+}
+
+function useraccounts(){
+    window,location = "useraccounts.html";
+}
+
+function products(){
+    window.location = "products.html";
+}
+
+function SellerAccounts(){
+    window.location = "SellerAccounts.html";
+}
